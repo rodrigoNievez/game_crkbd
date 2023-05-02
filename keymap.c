15ff2b5d937954c0286a78775d7b42ff49bd9db4
Rodrigo <rodrigo.nievez@gmail.com>
2023-05-02 00:49:50 -0600

    fixed bugs

--- keymap.c
+++ keymap.c
@@ -15,10 +15,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
-#include <stdio.h>
 
 enum layers {
-    _QUERTY,
+    _QWERTY,
     _GAME,
     _LOWER,
     _RAISE,
@@ -27,9 +26,10 @@ enum layers {
 };
 
 enum keycodes {
-    QUERTY = SAFE_RANGE,
+    QWERTY = SAFE_RANGE,
     GAME
 };
+
 
 #define WIN_NUM LT(_NUM, KC_LGUI)
 #define LSFT_ESC LSFT_T(KC_ESC)
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                                     ----------------------------  ----------------------------
      */
 
-    [_QUERTY] = LAYOUT_split_3x6_3(
+    [_QWERTY] = LAYOUT_split_3x6_3(
         LSFT_ESC, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC ,
         KC_TAB  , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_ENT  ,
         KC_LCTL , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                     KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT ,
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV  , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                         KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS ,
         KC_F1   , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,                         KC_HOME, KC_UP  , KC_END , KC_EQL , KC_LBRC, KC_RBRC ,
         KC_F7   , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,                         KC_LEFT, KC_DOWN, KC_RIGHT,XXXXXXX, KC_QUOT, KC_NUHS ,
-                                             XXXXXXX, XXXXXXX, ______ ,        KC_RSFT, KC_RALT, KC_ALGR
+                                             XXXXXXX, XXXXXXX, XXXXXXX,        KC_RSFT, KC_RALT, KC_ALGR
     ),
     
     /* _RAISE
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         S(KC_GRV)   , S(KC_1)   , S(KC_2)   , S(KC_3)   , S(KC_4), S(KC_5)   ,                       S(KC_6)      , S(KC_7)      , S(KC_8), S(KC_9)  , S(KC_0)   , S(KC_MINS),
         ALGR(KC_GRV), ALGR(KC_1), ALGR(KC_2), ALGR(KC_3), KC_NUBS, S(KC_NUBS),                       ALGR(KC_LBRC), ALGR(KC_RBRC), XXXXXXX, S(KC_EQL), S(KC_LBRC), S(KC_RBRC),
         XXXXXXX     , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX, XXXXXXX   ,                       ALGR(KC_QUOT), ALGR(KC_NUHS), XXXXXXX, XXXXXXX  , S(KC_QUOT), S(KC_NUHS),
-                                                          XXXXXXX, XXXXXXX   , XXXXXXX,        ______ , XXXXXXX   , XXXXXXX
+                                                          XXXXXXX, XXXXXXX   , XXXXXXX,        XXXXXXX, XXXXXXX   , XXXXXXX
     ),
 
     /* _NUM
@@ -160,6 +160,26 @@ uint32_t layer_state_set_user(uint32_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case QWERTY:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_QWERTY);
+            }
+            return false;
+            break;
+        case GAME:
+            if (record->event.pressed) {
+                rgblight_setrgb(RGB_WHITE);
+                set_single_persistent_default_layer(_GAME);
+            }
+            return false;
+            break;
+    }
+
+    return true;
+}
+
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   if (!is_keyboard_master()) {
@@ -171,7 +191,7 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 void oled_render_layer_state(void) {
     oled_write_P(PSTR("Layer: "), false);
     switch (layer_state) {
-        case _QUERTY:
+        case _QWERTY:
             oled_write_ln_P(PSTR("Qwerty"), false);
             break;
         case _GAME:
@@ -186,7 +206,7 @@ void oled_render_layer_state(void) {
         case _NUM:
             oled_write_ln_P(PSTR("Number"), false);
             break;
-        case _ADJUST::
+        case _ADJUST:
             oled_write_ln_P(PSTR("Adjust"), false);
             break;
     }
@@ -254,24 +274,4 @@ bool oled_task_user(void) {
     }
     return false;
 }
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case QWERTY:
-            if (record->event.pressed) {
-                rgblight_mode(RGBLIGHT_MODE_RAINBOW_SWIRL + 5);
-                set_single_persistent_default_layer(_QWERTY);
-            }
-            return false;
-            break;
-        case GAME:
-            if (record->event.pressed) {
-                rgblight_mode(RGBLIGHT_MODE_STATIC_GRADIENT);
-                set_single_persistent_default_layer(_GAME);
-            }
-            return false;
-            break;
-    }
-
-    return true;
-}
+#endif // OLED_ENABLE
